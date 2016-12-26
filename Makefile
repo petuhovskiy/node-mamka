e6f8042ead3127e67ec9283f5b8a90f8e73f8539
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2016-12-26 19:38:36 +0300

    app

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 init:
 	git clone https://github.com/Kickoman/mygithubpage.git static
 	cd static && git checkout gh-pages
+init-arthur:
+	git clone https://github.com/petuhovskiy/mygithubpage.git static
+	cd static && git checkout gh-pages
 update:
 	cd static && git pull
+run:
+	npm start
 .PHONY: init update
--- index.js
+++ index.js
@@ -1,12 +1,80 @@
 const express = require('express');
 const fs = require('fs');
+const os = require('os');
+const escape = require('escape-html');
+const marked = require('marked');
+const bodyParser = require('body-parser')
+
+const template = (strings, ...keys) => {
+  return ((...values) => {
+    const dict = values[values.length - 1] || {};
+    const result = [strings[0]];
+    keys.forEach((key, i) => {
+      const value = Number.isInteger(key) ? values[key] : dict[key];
+      result.push(value, strings[i + 1]);
+    });
+    return result.join('');
+  });
+};
 
 const app = express();
 
-app.on('/add', (req, res) => {
-	//TODO
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const index = 'static/index.html';
+
+const appendToIndex = (template, content) => {
+	const append_after = '<!--APPEND_POST_AFTER-->';
+
+	const pos = template.indexOf(append_after);
+	if (pos == -1) {
+		console.log('append block not found!');
+		return template;
+	}
+	const i = pos + append_after.length;
+	return template.slice(0, i) + os.EOL + os.EOL + content + template.slice(i);
+};
+
+const readIndex = () => {
+	return fs.readFileSync(index, 'utf8');
+};
+
+const writeIndex = (content) => {
+	return fs.writeFileSync(index, content);
+};
+
+const postTemplate = template`
+<h2>${0}</h2>
+
+${1}
+
+<blockquote><p>
+${2}
+</p></blockquote>
+`;
+
+const processTemplate = (title, content, tags) => {
+	const linesHtml = content.match(/[^\r\n]+/g);
+
+	let tagsHtml = '<span>' 
+		+ tags.split(';').map(escape).join('</span>;' 
+		+ os.EOL + '<span>') 
+		+ '</span>';
+
+	return postTemplate(escape(title), marked(content), tagsHtml);
+};
+
+const updateIndex = (content) => {
+	writeIndex(appendToIndex(readIndex(), content));
+};
+
+app.post('/add', urlencodedParser, (req, res) => {
+	console.log(req);
+	console.log(req.body);
+	const {title, content, tags} = req.body;
+	updateIndex(processTemplate(title, content, tags));
+	res.redirect('/');
 });
 
 app.use(express.static('static'));
-
-app.listen(80);
+app.listen(8080);
